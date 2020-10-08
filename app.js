01edcbf3f0121a4187dd44e0e011a015ef826f0d
TysonC Williams <tdub8@me.com>
2020-10-07 18:47:43 -0600

    play function, else/if, code clean up

--- app.js
+++ app.js
@@ -17,3 +17,41 @@ function setComputerSelection() {
 function play() {
   setComputerSelection()
 }
+
+function alertWin() {
+  window.alert('You Won!')
+}
+
+function alertLose {
+  window.alert('Sad Day')
+}
+
+function alertTie() {
+  window.alert('You Tied')
+}
+
+
+
+
+function play(){
+  switch (computerChoice) {
+  case 'rock':
+    if (playerChoice == 'rock') {
+      alertTie()
+    } else if (playerChoice == 'paper'){
+      alertWin()
+    } else {
+      alertLose()
+    }
+    
+    break;
+  case 'paper':
+    // code block
+    break;
+  case 'scissors':
+  // code block
+  default:
+    break;
+  // code block
+}
+
